e477abdac493d5223b82bf20997adab96ab1b843
Nick Chen <nick.chen@cmp.com.tw>
2025-10-07 09:16:17 +0800

    Initial commit

---
+++ src/main/java/com/niqdev/micro/MicroConfigNiqDevApplication.java
@@ -0,0 +1,13 @@
+package com.niqdev.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroConfigNiqDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroConfigNiqDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niqdev/micro/MicroConfigNiqDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niqdev.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroConfigNiqDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
